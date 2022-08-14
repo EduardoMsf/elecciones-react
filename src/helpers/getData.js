48b06153da04663b5e7504603fcbe129e0889a9d
Eduardo Samaniego <samaniegomsf@gmail.com>
2022-08-14 17:18:13 -0500

    Url de las imagenes de los canditatos

---
+++ src/helpers/getData.js
@@ -0,0 +1,12 @@
+
+
+const baseURL = `https://res.cloudinary.com/dheyjds01/image/upload/v1660511296/Candidatos/`
+
+export const getData = () =>{
+  const imageUrls = []
+  for(let index = 1; index<=20 ; index++){
+    imageUrls.push(`${baseURL}${index}.webp`)
+  }
+
+  return imageUrls
+}
